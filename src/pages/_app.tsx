e774877f6ed0e9776b274e354030e34858b856b7
Sebas <sheva3232@gmail.com>
2023-09-06 21:11:00 -0500

    cleaning app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Web3Auth } from '@web3auth/modal'
-import Web3 from "web3";
 import '@/styles/globals.css'
 import { useState } from 'react'
 import { User, UserContext } from '@/lib/UserContext'
@@ -8,70 +7,9 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import customTheme from '../theme/theme'
 import { Button, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react'
-import RPC from '../helper/RPCHelper'
-import { CHAIN_NAMESPACES } from "@web3auth/base";
 import { SafeEventEmitterProvider } from "@web3auth/base";
 export default function App({ Component, pageProps }: AppProps) {
-	const [web3auth, setWeb3auth] = useState<Web3Auth | null >(null);
-  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
-  const [address, setAddress] = useState("");
-  const [balance, setBalance] = useState("");
-  const [chainId, setChainId] = useState("");
-  const [userData, setUserData] = useState({});
-
-
 	const [user, setUser] = useState<User | null>(null)
-	useEffect(() => {
-		const init = async () => {
-      try {
-				if(process.env.NEXT_PUBLIC_YOUR_WEB3AUTH_CLIENT_ID){
-					const web3auth = new Web3Auth({
-						clientId: process.env.NEXT_PUBLIC_YOUR_WEB3AUTH_CLIENT_ID,
-						chainConfig: {
-							chainNamespace: CHAIN_NAMESPACES.EIP155,
-							chainId: "0x13881",
-							rpcTarget: "https://rpc-mumbai.maticvigil.com/",
-						},
-					});
-	
-					setWeb3auth(web3auth);
-					await web3auth.initModal();
-					setProvider(web3auth.provider);
-				}
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    init();
-	}, [])
-	const login = async () => {
-    if (!web3auth) {
-      console.log("web3auth not initialized yet");
-      return;
-    }
-    const web3authProvider = await web3auth.connect();
-    setProvider(web3authProvider);
-		console.log(web3authProvider);
-		
-  };
-	const getAccounts = async () => {
-		try {
-			if (!provider) {
-				console.log("provider not initialized yet");
-				return;
-			}
-			if(!web3auth) return
-			const aa = web3auth.provider 
-			if (!aa) return
-			const web3 = new Web3(aa);
-			const address = (await web3.eth.getAccounts())[0];
-			// console.log('address is', address);
-		} catch (error) {
-			console.log(error);
-		}
-    
-  };
 	return (
 		<ChakraProvider theme={customTheme}>
 			<UserContext.Provider value={{ user, setUser }}>
@@ -122,11 +60,10 @@ export default function App({ Component, pageProps }: AppProps) {
 									_hover={{
 										bg: 'blue.500'
 									}}
-                  onClick={login}
 								>
 									Create Project
 								</Button>
-								<Button rounded={'full'} onClick={getAccounts}>How It Works</Button>
+								<Button rounded={'full'}>How It Works</Button>
 							</Stack>
 						</Stack>
 					</Flex>
