b864fc1a7b05e695f18987eb39d4b426304c27ca
Christian Arevalo <tiancris200@gmail.com>
2023-09-24 00:41:41 -0500

    :lipstick: Added favicon and changed carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home() {
 				<title>{metadata.title}</title>
 				<meta name='description' content={metadata.description} />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<link rel='icon' href='/img/favicon.ico' sizes='any' />
+				<link rel='icon' href='/images/favicon.ico' sizes='any' />
 			</Head>
 			<Navbar />
 			<Hero />
