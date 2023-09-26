7226b3bb8ce7c724b472efc3c639041d86e4af1f
salviega <salviega6@gmail.com>
2023-09-26 09:44:50 -0500

    :see_no_evil: Was removed .gitignore cache

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
-export const CHAINID = 137
-export const PROVIDER = `https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_POLYGON_ALCHEMY_API_KEY}`
+export const CHAINID = process.env.NEXT_PUBLIC_CHAIN_ID
+export const PROVIDER = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
 export const SCAN = (
 	address: string,
-	scan: string = `https://polygonscan.com/address`
+	scan: string | undefined = process.env.NEXT_PUBLIC_EVM_EXPLORER
 ) => `${scan}/${address}`
